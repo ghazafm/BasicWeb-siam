e0ec06f466ccd9a8c106c7d047f9aaee1e7f55fc
ghazafm <fauzan.ghaza@gmail.com>
2024-04-17 10:54:01 +0700

    add routes student

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\StudentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/page', [StudentController::class, 'home']);
+Route::get('/page/about', [StudentController::class, 'about']);
+Route::get('/page/contact', [StudentController::class, 'contact']);
+
+Route::get('/student', [StudentController::class, 'index']);
+Route::get('/student/add', [StudentController::class, 'add']);
+Route::post('/student/addsave', [StudentController::class, 'addsave']);
+Route::get('/student/edit/{nim}', [StudentController::class, 'edit']);
+Route::post('/student/editsave', [StudentController::class, 'editsave']);
+Route::get('/student/delete/{nim}', [StudentController::class, 'delete']);
