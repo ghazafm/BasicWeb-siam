a7dbd3358aa006466c201f9ad33a50454ee504eb
ghazafm <fauzan.ghaza@gmail.com>
2024-04-17 16:58:59 +0700

    add StudentController

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -1,43 +1,65 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Facades\DB;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class StudentController extends Controller
 {
     public function index()
     {
-        // mengambil data mahasiswa ke view index
-        $mahasiswa = DB::table('mahasiswa')->get();
-
-        // mengirim data mahasiswa ke view index
-        return view('student.index',['mahasiswa' => $mahasiswa]);
+    	// mengambil data dari table mahasiswa
+    	$mahasiswa = DB::table('mahasiswa')->get();
+ 
+    	// mengirim data mahasiswa ke view index
+    	return view('student.index',['mahasiswa' => $mahasiswa]);
+ 
     }
 
-    public function add(){
-        return view('student.add');
-    }
+	public function add() 
+	{
+		return view('student.add');
+	}
 
-    public function addsave(Request $req){
+	public function addsave(Request $req)
+	{
+		// insert data ke table mahasiswa
+		DB::table('mahasiswa')->insert([
+			'nama' => $req->nama,
+			'alamat' => $req->alamat,
+			'prodi' => $req->prodi,
+			'ipk' => $req->ipk
+		]);
+		// alihkan halaman ke halaman mahassiwa
+		return redirect('/student');
+	}
 
-        // insert data ke table mahasiswa
-        DB::table('mahasiswa')->insert([
-            'nama' => $req->nama,
-            'alamat' => $req->alamat,
-            'notelp' => $req->notelp,
-            'ipk' => $req->ipk
-        ]);
-        // alihkan halaman ke halaman mahasiswa
-        return redirect('/student');
-    }
+	public function edit($nim)
+	{
+		// mengambil data mahasiwa berdasarkan nim yang dipilih
+		$mahasiswa = DB::table('mahasiswa')->where('nim',$nim)->get();
+		// mengirim data mahasiwa yang didapat ke view edit.blade.php
+		return view('student.edit',['mahasiswa' => $mahasiswa]);
+	}
 
-    public function edit($nim){
-        // mengambil data mahasiswa berdasarkan nim yang dipilih
-        $mahasiswa = DB::table('mahasiswa')->where('nim',$nim)->get();
+	public function editsave(Request $req)
+	{
+		// update data mahasiswa
+		DB::table('mahasiswa')->where('nim',$req->nim)->update([
+			'nama' => $req->nama,
+			'alamat' => $req->alamat,
+			'prodi' => $req->prodi,
+			'ipk' => $req->ipk
+		]);
+		// alihkan halaman data mahasiswa 
+		return redirect('/student');
+	}
 
-        // mengirim data mahasiswa yang didapat ke view edit.blade.php
-        return view('student.edit',['mahasiswa' => $mahasiswa]);
-    }
+	public function delete($nim)
+	{
+		DB::table('mahasiswa')->where('nim', $nim)->delete();
+
+		return redirect('student');
+	}
 }
